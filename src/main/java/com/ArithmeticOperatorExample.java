6373e33a143083467e1bf216ad751892c64c742b
parkminseob <minsub.prk@gmail.com>
2020-07-29 21:02:15 +0900

    혼공자

---
+++ src/main/java/com/ArithmeticOperatorExample.java
@@ -0,0 +1,31 @@
+package com;
+
+public class ArithmeticOperatorExample {
+    public static void main(String[] args) {
+      
+      
+      int v1 = 5;
+      int v2 = 2;
+      
+      int result = v1 + v2;
+      System.out.println(result);
+      
+      int result2 = v1 - v2;
+      System.out.println("result2 = " + result2 );
+      
+      int result3 = v1 * v2;
+      System.out.println("result3 = " + result3);
+      
+      int result4 = v1 / v2;
+      System.out.println("result4 = " + result4);
+      
+      int result5 = v1 % v2;
+      System.out.println("result5 = " + result5);
+      
+      double result6 = (double) v1 / v2 ;
+      System.out.println("result6 = " + result6);
+      
+      
+    }
+  
+}
---
+++ src/main/java/com/DenyLogicOperatorExaple.java
@@ -0,0 +1,20 @@
+package com;
+
+public class DenyLogicOperatorExaple {
+    public static void main(String[] args) {
+      
+      boolean play = true;
+      
+      System.out.println(play);
+      
+      play = !play;
+      System.out.println(play);
+      
+      play = !play;
+      System.out.println(play);
+      
+      
+    }
+    
+
+}
