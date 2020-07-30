9475671c25662a3102619b52f6513510115c7b77
parkminseob <minsub.prk@gmail.com>
2020-07-31 00:08:17 +0900

    .

--- src/main/java/com/Practice001.java
+++ src/main/java/com/Practice001.java
@@ -1,12 +1,22 @@
 package com;
-
+import java.util.Scanner;
 public class Practice001 {
  public static void main(String[] args) {
-
-   int A, B, C;
-
-
-
-
-}
+   Scanner scanner = new Scanner(System.in);
+   
+   int A;
+   int B;
+   
+   A = scanner.nextInt();
+   B = scanner.nextInt();
+       
+   
+   if (A > B) {
+     System.out.println(">");
+   } else if (A < B) {
+     System.out.println("<");
+   } else
+     System.out.println("==");
+   
+ }
 }
