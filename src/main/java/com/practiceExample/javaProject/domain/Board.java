b9ac175c7b444b1579303c97e7b3a64ee791c352
parkminseob <minsub.prk@gmail.com>
2020-08-21 21:43:02 +0900

    m

--- src/main/java/com/practiceExample/javaProject/domain/Board.java
+++ src/main/java/com/practiceExample/javaProject/domain/Board.java
@@ -3,10 +3,43 @@ package com.practiceExample.javaProject.domain;
 import java.sql.Date;
 
 public class Board {
-  public int no;
-  public String title;
-  public String content;
-  public String writer;
-  public int viewCount;
-  public Date registeredDate;
+  private int no;
+  private String title;
+  private String writer;
+  private int viewCount;
+  private Date registeredDate;
+  
+  
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getWriter() {
+    return writer;
+  }
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+  public int getViewCount() {
+    return viewCount;
+  }
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
+
+  
 }
