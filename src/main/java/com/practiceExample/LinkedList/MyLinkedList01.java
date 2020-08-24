1168e328f42b954365832bfeb8612eb2cbf84c4e
parkminseob <minsub.prk@gmail.com>
2020-08-24 21:15:54 +0900

    8/24

---
+++ src/main/java/com/practiceExample/LinkedList/MyLinkedList01.java
@@ -0,0 +1,6 @@
+package com.practiceExample.LinkedList;
+
+// 1) LinkedList 클래스 정의  
+public class MyLinkedList01 {
+ 
+}
---
+++ src/main/java/com/practiceExample/LinkedList/MyLinkedList05.java
@@ -0,0 +1,48 @@
+package com.practiceExample.LinkedList;
+
+// 1) LinkedList 클래스 정의  
+// 2) 값을 담을 노드 클래스 설계
+// 3) 첫 번째 노드와 마지막 노드의 주소를 담을 필드를 추가한다.
+//    목록 크기를 저장할 필드를 추가한다.
+// 4) 목록에 값을 추가하는 add()메서드 정의.
+// 5) 목록에 값을 조회하는 get()메서드 정의.
+public class MyLinkedList05 {
+ 
+  // Node 클래스란?
+  // 목록에서 각 항목의 값을 보관하는 객체 역할을 수행한다.
+  // 여러 개의 MyLinkedList 객체가 공유하는 클래스이므로
+  // 스태틱으로 Node 클래스를 설계한다.
+  Node first; 
+  Node last;
+  int size; //목록크기 보관 
+  
+  static class Node{
+    Object value;
+    Node next;
+  }
+  
+  public boolean add(Object e) {
+    Node node = new Node();
+    node.value = e;
+    if(first == null) {
+      first = node;
+    } else {
+      last.next = node;
+    }
+    last = node;
+    
+    size++;
+    return true;
+  }
+  
+  public Object get(int index) {
+    if(index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("유효하지 않은 인덱스 값");
+    }
+    Node cursor = this.first;
+    for(int i = 0; i < index; i++) {
+      cursor = cursor.next;
+    }
+    return cursor.value;
+  }
+}
