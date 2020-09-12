99fa992b67455a7e91888cd9af81360bdf434c58
parkminseob <minsub.prk@gmail.com>
2020-09-12 18:55:12 +0900

    algorithm

---
+++ src/main/java/com/baekjoon/backjoon2443.java
@@ -0,0 +1,24 @@
+package com.baekjoon;
+
+import java.util.Scanner;
+
+public class backjoon2443 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int star = scanner.nextInt();
+
+    for(int i = 1 ; i <= star; i++) {
+      //공백찍기
+      for(int j = 1; j < i; j++) {
+        System.out.print(" ");
+      }
+      //별찍기
+      for(int k = (2*star)-(i*2-1); k > 0; k--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    scanner.close();
+  }
+}
---
+++ src/main/java/com/baekjoon/backjoon2445.java
@@ -0,0 +1,37 @@
+package com.baekjoon;
+
+import java.util.Scanner;
+
+public class backjoon2445 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int star = scanner.nextInt();
+    for(int i = 1 ; i <= star; i++) {
+      for(int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      for(int k = 2*star-i; k > 0; k--) {
+        if(k>i) {
+          System.out.print(" ");
+        }else {
+          System.out.print("*");
+        }
+      }
+      System.out.println();
+    }
+
+    for(int i = 2; i <= star; i++) {
+      for(int j = i; j <= star; j++) {
+        System.out.print("*");
+      }
+      for(int k = 1; k < i*2-1; k++) {
+        System.out.print(" ");
+      }
+      for(int h = star; h >= i; h--) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    scanner.close();
+  }
+}
